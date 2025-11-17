835793fe1508595368c5c4f8d444f153678bad99
wuhaiyue <wuhaiyue@leyaoyao.com>
2025-11-17 18:26:03 +0800

    fix: 更新 Docusaurus 配置中的 baseUrl 和 projectName

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,12 +18,12 @@ const config: Config = {
   url: 'https://wuhy5.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ying-ju/',
+  baseUrl: '/ying-ju-crawler-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Wuhy5', // Usually your GitHub org/user name.
-  projectName: 'ying-ju', // Usually your repo name.
+  projectName: 'ying-ju-crawler-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
